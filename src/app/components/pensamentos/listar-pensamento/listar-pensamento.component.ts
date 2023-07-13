9cb1f828cc85e8e52b9e8aa7a5cc20aa3ede80d9
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2023-07-13 18:09:08 -0300

    Components communication - ngFor

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    }, 
+    {
+      conteudo: 'Minha propriedade é decorada com o @input()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
